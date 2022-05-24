c4ddc3898c01756d07eeb79dc4f0da34d1f558fc
DhairyaChandrani <dhairyachandrani0706@gmail.com>
2022-05-24 10:57:30 +0530

    main

---
+++ 1.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this code
+    def countOnes(self,arr, N):
+        #Your code here
+        count = 0
+        for i in arr:
+            if i==1:
+                count += 1
+        return count
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            ob=Solution()
+            print(ob.countOnes(A,N))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
