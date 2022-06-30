a9fa7265e97436e88b0712cd7592f18182351e88
DhairyaChandrani <dhairyachandrani0706@gmail.com>
2022-06-30 23:39:06 +0530

    main

---
+++ circular linked list/Circular Linked List Head Insert.py
@@ -0,0 +1,78 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+def insertInHead(head,x):
+    #code here
+    temp = Node(x)
+    if head == None:
+        temp.next = temp
+        return temp
+    else:
+        temp.next = head.next
+        head.next = temp
+        head.data,temp.data = temp.data,head.data
+        return head
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+# contributed by RavinderSinghPB
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+
+def displayList(head):
+    t=head
+    while t.next!=head:
+        print(t.data,end=' ')
+        t=t.next
+    print(t.data,end=' ')
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for tcs in range(t):
+
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        data=int(input())
+
+        ll = Llist()
+        tail = None
+
+        for nodeData in arr:
+            tail = ll.insert(nodeData, tail)
+        #making circular
+        tail.next=ll.head
+
+        resHead=insertInHead(ll.head,data)
+        displayList(resHead)
+        print()
+# } Driver Code Ends
