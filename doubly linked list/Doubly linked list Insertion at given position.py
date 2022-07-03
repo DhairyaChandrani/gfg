86155577af0d37b0620f553554de1fdc7dc2235a
DhairyaChandrani <dhairyachandrani0706@gmail.com>
2022-07-04 00:24:14 +0530

    main

--- doubly linked list/Doubly linked list Insertion at given position.py
+++ doubly linked list/Doubly linked list Insertion at given position.py
@@ -1,13 +1,12 @@
-# Your task is to complete this function
 # function should add a new node after the pth position
 # function shouldn't print or return any data
 
 '''
 class Node:
-	def __init__(self, data):
-		self.data = data
-		self.next = None
-		self.prev = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
 '''
 #Function to insert a new node at given position in doubly linked list.
@@ -42,41 +41,41 @@ def addNode(head, p, data):
 #{ 
 #  Driver Code Starts
 class Node:
-	def __init__(self, data):
-		self.data = data
-		
-		
-		self.next = None
-		self.prev = None
+    def __init__(self, data):
+        self.data = data
+        
+        
+        self.next = None
+        self.prev = None
 
 
 class DoublyLinkedList:
-	def __init__(self):
-		self.head = None
-		self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-	def add(self, new_data):
-		new_node = Node(new_data)
-		if(self.head == None):
-		    self.head = new_node
-		    self.tail = new_node
-		    return
-		
-		self.tail.next = new_node
-		new_node.prev = self.tail
-		self.tail = new_node
-		return
-		
-	def printList(self, node):
-		while(node.next is not None):
-			node = node.next
-		while node.prev is not None:
-		    node = node.prev
-		while(node is not None):
-		    print(node.data, end=" ")
-		    node = node.next
-		print()
-		
+    def add(self, new_data):
+        new_node = Node(new_data)
+        if(self.head == None):
+            self.head = new_node
+            self.tail = new_node
+            return
+        
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        return
+        
+    def printList(self, node):
+        while(node.next is not None):
+            node = node.next
+        while node.prev is not None:
+            node = node.prev
+        while(node is not None):
+            print(node.data, end=" ")
+            node = node.next
+        print()
+        
 if __name__=='__main__':
     t = int(input())
     for i in range(t):
